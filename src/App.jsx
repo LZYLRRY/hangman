ae28515ace23a4d51a789b62f06d18d08af7734c
LZYLRRY <162039530+LZYLRRY@users.noreply.github.com>
2025-03-05 14:33:13 +0100

    Init project hangman

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <main className="main-container">
+        <header className="header">
+          <h1 className="header__title">Assembly: Endgame</h1>
+          <p className="header__description">
+            Guess the word in under 8 attempts to keep the programming world
+            safe from Assembly!
+          </p>
+        </header>
+      </main>
+    </>
+  );
+}
+
+export default App;
